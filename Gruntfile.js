405c680ce57925fea9cbf66aee3e05c3d76e6a18
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-05-22 16:13:12 +0100

    Modified

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,5 +2,46 @@
 
 module.exports = function(grunt) {
 
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-};
+  var pkg = grunt.file.readJSON('package.json');
+
+  var js = {
+    core: [
+      'execute.js',
+      'systems.js',
+      'system.js',
+      'system/index.js',
+    ],
+  }; // EOF js
+
+  var config = {
+
+    pkg: pkg,
+
+    jscs: {
+      options: {
+        config: '.jscsrc',
+      }, // EOF options
+      src: [
+        // Systems js:
+        './systems.js',
+        './execute.js',
+
+        // All System js:
+        'system/**/*.js',
+
+        // Tests:
+        'test/**/*.js',
+      ], // EOF src
+    },  // EOF jscs
+
+  }; // EOF config
+
+  grunt.initConfig(config);
+
+  grunt.registerTask('test:code-style', ['jscs']);
+  grunt.registerTask('test', ['test:code-style']);
+
+}; // EOF module
--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 The Systems Subtree
 
 NOTE: Look at Diagrams SubTree how to implement tests (including code-style).
+
+# Test
+
+To test the javascript files for code-style errors, run the following command:
+
+````
+grunt test
+````
